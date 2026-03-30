533402b78148013323f3bb296db5bd23e8409835
Midhun P M <midhun.p@webandcrafts.com>
2026-03-30 10:34:29 +0530

    deploy

---
+++ backend/config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => env('CORS_ALLOWED_ORIGINS', '*') === '*'
+        ? ['*']
+        : explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
